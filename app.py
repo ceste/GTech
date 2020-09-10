987f71b509e7cc1b838ea23d01036af4fbbf5682
ceste <chandra.sutrisno@gmail.com>
2020-09-10 10:15:00 +0700

    update app

--- app.py
+++ app.py
@@ -56,6 +56,6 @@ def predict():
 	return jsonify(results=output)
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 5000))
+	port = int(os.environ.get("PORT", 33507))
 	app.run(host='0.0.0.0', port = port, debug=True)
 
